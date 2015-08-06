20bd5bf03c98be8ce7276c1e558f49a2cc96c64f
tunnckoCore <mameto_100@mail.bg>
2015-08-07 00:42:37 +0300

    use extend-shallow

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var fs = require('fs');
 var path = require('path');
 var MiniHandlebars = require('mini-handlebars');
 var hbs = new MiniHandlebars();
+var extend = require('extend-shallow');
 
 exports.name = 'mini-handlebars';
 exports.inputFormats = ['hbs', 'mhbs', 'handlebars'];
 exports.outputFormat = 'html';
 
 exports.render = function _render(str, options, locals) {
-  return hbs.render(str, locals);
+  var data = extend({}, options, locals);
+  return hbs.render(str, data);
 };
